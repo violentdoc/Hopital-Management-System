22819d3e8dcf28638aff651338af113c744f9e7d
violentdoc <violentdoc@gmail.com>
2021-07-06 21:53:07 +0600

    Initial commit

---
+++ Hospital Management System/src/home/AddPatient.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package home;
+
+/**
+ *
+ * @author acer
+ */
+public class AddPatient {
+
+    public AddPatient() {
+    }
+    
+}
